2bdb4c16b4a81f3493c3b2bcc7327049013d9731
Annie Nguyen <anguyen.aln@gmail.com>
2021-11-29 14:16:41 -0800

    added flaticon and cards

--- assets/js/food2.js
+++ assets/js/food2.js
@@ -17,48 +17,6 @@
 //   }
 // }
 
-var genre = "/chart/popular/genre/horror";
-
-$.ajax(
-  "https://imdb8.p.rapidapi.com/title/get-popular-movies-by-genre?genre=" +
-    genre,
-  {
-    method: "GET",
-    headers: {
-      "x-rapidapi-host": "imdb8.p.rapidapi.com",
-      "x-rapidapi-key": "2fd256b7eamsh398f55bc9d76f1dp1ede28jsnaaaff957db6c",
-    },
-  }
-)
-  .then((response2) => {
-    console.log(response2);
-
-    let title = response2[8].split("/");
-    $.ajax(
-      "https://imdb8.p.rapidapi.com/title/get-details?tconst=" +
-        title[2] +
-        "&limit=25&region=US",
-      {
-        method: "GET",
-        headers: {
-          "x-rapidapi-host": "imdb8.p.rapidapi.com",
-          "x-rapidapi-key":
-            "2fd256b7eamsh398f55bc9d76f1dp1ede28jsnaaaff957db6c",
-        },
-      }
-    )
-      .then((response3) => {
-        console.log(response3);
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  })
-  .catch((err) => {
-    console.error(err);
-  });
-
-var genre = "/chart/popular/genre/horror";
 let ingredient = [
   "oatmeal",
   "seafood pasta",
@@ -79,30 +37,30 @@ let ingredient = [
 
 // }
 
-function fetchRecipes(ingredient) {
-  $.ajax(
-    "https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&tags=under_30_minutes&q=" +
-      ingredient,
-    {
-      method: "GET",
-      headers: {
-        "x-rapidapi-host": "tasty.p.rapidapi.com",
-        "x-rapidapi-key": "2fd256b7eamsh398f55bc9d76f1dp1ede28jsnaaaff957db6c",
-      },
-    }
-  )
-    .then((response2) => {
-      console.log(response2);
+// function fetchRecipes(ingredient) {
+//   $.ajax(
+//     "https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&tags=under_30_minutes&q=" +
+//       ingredient,
+//     {
+//       method: "GET",
+//       headers: {
+//         "x-rapidapi-host": "tasty.p.rapidapi.com",
+//         "x-rapidapi-key": "2fd256b7eamsh398f55bc9d76f1dp1ede28jsnaaaff957db6c",
+//       },
+//     }
+//   )
+//     .then((response2) => {
+//       console.log(response2);
 
-      let results = response2["results"];
-      for (let i = 0; i < results.length; ++i) {
-        console.log(results[i]["name"]);
-      }
-    })
-    .catch((err) => {
-      console.error(err);
-    });
-}
+//       let results = response2["results"];
+//       for (let i = 0; i < results.length; ++i) {
+//         console.log(results[i]["name"]);
+//       }
+//     })
+//     .catch((err) => {
+//       console.error(err);
+//     });
+// }
 
 // /* Drop Down Menu for Moods*/
 // function myFunction() {
