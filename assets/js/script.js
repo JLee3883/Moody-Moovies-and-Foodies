31d7f8f89516e7beca6420ae5b61872d486a69e7
JLee <thishereisjenn@gmail.com>
2021-12-01 15:40:00 -0800

    working on recipe. image functioning

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,144 +1,205 @@
-
-// var moodTypes = {
-//     happy: ['pizza', 'food type 2'],
-//     sad: ['food type 1', 'food type 2', 'food type 3'],
-//     deptressed: ['food type 1']
-// }
-
-// create hero section with title and site description
-// create interface for getting user mood (i.e. dropdown? or user enters)
-// take user mood select value
-// get that value to fetch set genres we made for that mood
-// do the same for food recipes API
-// what data points are we going to keep? posters? details form the fetch request. how to render those in our html
+/* Drop Down Menu for Moods*/
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+  
+  // Dropdown menu closes if user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-cont");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+}
 
 function toggleTense() {
-    genreRequest("/chart/popular/genre/family","#movie1");
-
-    setTimeout(genreRequest("/chart/popular/genre/musical","#movie2"),1000);
-   
-    setTimeout(genreRequest("/chart/popular/genre/comedy","#movie3"),2000);   
+    // genreRequest("/chart/popular/genre/family","#movie1");
+    // setTimeout(genreRequest("/chart/popular/genre/musical","#movie2"),1000);
+    // setTimeout(genreRequest("/chart/popular/genre/comedy","#movie3"),2000);  
+    
+    recipeRequest("recipes/detail?id=7577");
     
 }
 
-function toggleIrritated() {
-    genreRequest("/chart/popular/genre/mystery","#movie1");
+// function toggleIrritated() {
+//     genreRequest("/chart/popular/genre/mystery","#movie1");
 
-    setTimeout(genreRequest("/chart/popular/genre/thriller","#movie2"),1000);
+//     setTimeout(genreRequest("/chart/popular/genre/thriller","#movie2"),1000);
    
-    setTimeout(genreRequest("/chart/popular/genre/drama","#movie3"),2000);
+//     setTimeout(genreRequest("/chart/popular/genre/drama","#movie3"),2000);
       
-}
+// }
 
-function toggleRelax() {
-    genreRequest("/chart/popular/genre/comedy","#movie1");
+// function toggleRelax() {
+//     genreRequest("/chart/popular/genre/comedy","#movie1");
 
-    setTimeout(genreRequest("/chart/popular/genre/fantasy","#movie2"),1000);
+//     setTimeout(genreRequest("/chart/popular/genre/fantasy","#movie2"),1000);
 
-    setTimeout(genreRequest("/chart/popular/genre/history","#movie3"),2000);
-}
+//     setTimeout(genreRequest("/chart/popular/genre/history","#movie3"),2000);
+// }
 
-function toggleDare() {
-    genreRequest("/chart/popular/genre/horror","#movie1");
+// function toggleDare() {
+//     genreRequest("/chart/popular/genre/horror","#movie1");
 
-    setTimeout(genreRequest("/chart/popular/genre/thriller","#movie2"),1000);
+//     setTimeout(genreRequest("/chart/popular/genre/thriller","#movie2"),1000);
 
-    setTimeout(genreRequest("/chart/popular/genre/mystery","#movie3"),2000);
-}
+//     setTimeout(genreRequest("/chart/popular/genre/mystery","#movie3"),2000);
+// }
 
-function toggleGloomy() {
-    genreRequest("/chart/popular/genre/sci-fi","#movie1");
+// function toggleGloomy() {
+//     genreRequest("/chart/popular/genre/sci-fi","#movie1");
 
-    setTimeout(genreRequest("/chart/popular/genre/mystery","#movie2"),1000);
+//     setTimeout(genreRequest("/chart/popular/genre/mystery","#movie2"),1000);
 
-    setTimeout(genreRequest("/chart/popular/genre/musical","#movie3"),2000);
-}
+//     setTimeout(genreRequest("/chart/popular/genre/musical","#movie3"),2000);
+// }
 
-function toggleExcited() {
-    genreRequest("/chart/popular/genre/action","#movie1");
+// function toggleExcited() {
+//     genreRequest("/chart/popular/genre/action","#movie1");
 
-    setTimeout(genreRequest("/chart/popular/genre/musical","#movie2"),1000);
+//     setTimeout(genreRequest("/chart/popular/genre/musical","#movie2"),1000);
 
-    setTimeout(genreRequest("/chart/popular/genre/mystery","#movie3"),2000);
-}
+//     setTimeout(genreRequest("/chart/popular/genre/mystery","#movie3"),2000);
+// }
 
-function toggleCheerful() {
-    genreRequest("/chart/popular/genre/comedy","#movie1");
+// function toggleCheerful() {
+//     genreRequest("/chart/popular/genre/comedy","#movie1");
 
-    setTimeout(genreRequest("/chart/popular/genre/animation","#movie2"),1000);
+//     setTimeout(genreRequest("/chart/popular/genre/animation","#movie2"),1000);
 
-    setTimeout(genreRequest("/chart/popular/genre/adventure","#movie3"),2000);
-}
+//     setTimeout(genreRequest("/chart/popular/genre/adventure","#movie3"),2000);
+// }
 
-function toggleDate() {
-    genreRequest("/chart/popular/genre/romance","#movie1");
+// function toggleDate() {
+//     genreRequest("/chart/popular/genre/romance","#movie1");
 
-    setTimeout(genreRequest("/chart/popular/genre/drama","#movie2"),1000);
+//     setTimeout(genreRequest("/chart/popular/genre/drama","#movie2"),1000);
 
-    setTimeout(genreRequest("/chart/popular/genre/horror","#movie3"),2000);
-}
+//     setTimeout(genreRequest("/chart/popular/genre/horror","#movie3"),2000);
+// }
 
-async function genreRequest(genre,id){
-    await $.ajax("https://imdb8.p.rapidapi.com/title/get-popular-movies-by-genre?genre=" + genre, {
-    "method": "GET",
-    "headers": {
-    "x-rapidapi-host": "imdb8.p.rapidapi.com",
-    "x-rapidapi-key": "6367609f03mshe6a3e9c1f4e6ba0p12185cjsn01a783ae2181"
-    }
+// async function genreRequest(genre,id){
+//     await $.ajax("https://imdb8.p.rapidapi.com/title/get-popular-movies-by-genre?genre=" + genre, {
+//     "method": "GET",
+//     "headers": {
+//     "x-rapidapi-host": "imdb8.p.rapidapi.com",
+//     "x-rapidapi-key": "6367609f03mshe6a3e9c1f4e6ba0p12185cjsn01a783ae2181"
+//     }
+//     })
+//     .then(async response2 => {
+//     console.log(response2[0]);
+
+//         let title = response2[8].split("/")
+//         await $.ajax("https://imdb8.p.rapidapi.com/title/get-details?tconst="+ title[2] +"&limit=25&region=US", {
+//             "method": "GET",
+//             "headers": {
+//             "x-rapidapi-host": "imdb8.p.rapidapi.com",
+//             "x-rapidapi-key": "6367609f03mshe6a3e9c1f4e6ba0p12185cjsn01a783ae2181"
+//             }
+//         })
+//         .then(response3 => {
+//             console.log(response3);
+//             console.log(id);
+//             var movieContainer = document.querySelector(id);
+//             movieContainer.innerHTML= "";
+//             var movieCard = document.createElement("div");
+//             movieCard.setAttribute("class", "card");
+//             var movieCardImage = document.createElement("div");
+//             movieCardImage.setAttribute("class", "card-image");
+//             var movieImage = document.createElement("img");
+//             movieImage.setAttribute("class", "cardImage");
+//             movieImage.setAttribute("src", response3.image.url);
+//             document.createElement("div");
+//             movieCardImage.appendChild(movieImage);
+//             movieCard.appendChild(movieCardImage);
+//             movieContainer.appendChild(movieCard);
+
+//         })
+//         .catch(err => {
+//             console.error(err);
+//         });
+//     })
+//     .catch(err => {
+//     console.error(err);
+// });
+// }
+
+async function recipeRequest(name){
+  await fetch("https://tasty.p.rapidapi.com/recipes/detail?" + name, {
+  "method": "GET",
+  "headers": {
+  "x-rapidapi-host": "tasty.p.rapidapi.com",
+  "x-rapidapi-key": "6367609f03mshe6a3e9c1f4e6ba0p12185cjsn01a783ae2181"
+  }
+  })
+  .then(async response2 => {
+      if (response2.ok)return response2.json();
+      console.log(response2[0]);
     })
-    .then(async response2 => {
-    console.log(response2[0]);
 
-        let title = response2[8].split("/")
-        await $.ajax("https://imdb8.p.rapidapi.com/title/get-details?tconst="+ title[2] +"&limit=25&region=US", {
-            "method": "GET",
-            "headers": {
-            "x-rapidapi-host": "imdb8.p.rapidapi.com",
-            "x-rapidapi-key": "6367609f03mshe6a3e9c1f4e6ba0p12185cjsn01a783ae2181"
-            }
-        })
-        .then(response3 => {
-            console.log(response3);
-            console.log(id);
-            var movieContainer = document.querySelector(id);
-            movieContainer.innerHTML= "";
-            var movieCard = document.createElement("div");
-            movieCard.setAttribute("class", "card");
-            var movieCardImage = document.createElement("div");
-            movieCardImage.setAttribute("class", "card-image");
-            var movieImage = document.createElement("img");
-            movieImage.setAttribute("class", "cardImage");
-            movieImage.setAttribute("src", response3.image.url);
-            document.createElement("div");
-            movieCardImage.appendChild(movieImage);
-            movieCard.appendChild(movieCardImage);
-            movieContainer.appendChild(movieCard);
+    //   let name = response2[8].split("/")
+    //   await $.ajax("https://tasty.p.rapidapi.com/recipes/detail?&=&id="+ name[2],{
+    //       "method": "GET",
+    //       "headers": {
+    //       "x-rapidapi-host": "tasty.p.rapidapi.com",
+    //       "x-rapidapi-key": "6367609f03mshe6a3e9c1f4e6ba0p12185cjsn01a783ae2181"
+    //       }
+    //   })
 
-        })
-        .catch(err => {
-            console.error(err);
-        });
-    })
-    .catch(err => {
-    console.error(err);
-});
+    // "<img src="+ recipe.thumbnail_url + " /> ";
+      .then(response3 => {
+          console.log(response3.results);
+         for (let i = 0; i < response3.results.length; i++) {
+             const recipe = response3.results[i];
+             var recipeContainer = document.querySelector("#recipeWrapper");
+          recipeContainer.innerHTML= "";
+        //   var recipeCard = document.createElement("div");
+        //   recipeCard.setAttribute("class", "card");
+        //   var recipeCardImage = document.createElement("div");
+        //   recipeCardImage.setAttribute("class", "card-image");
+        //   var recipeImage = document.createElement("img");
+        //   recipeImage.setAttribute("class", "cardImage");
+        //   recipeImage.setAttribute("img", recipe.thumbnail_url);
+        //   document.createElement("div");
+        //   recipeCardImage.appendChild(recipeImage);
+        //   recipeCard.appendChild(recipeCardImage);
+        //   recipeContainer.appendChild(recipeCard);;
+
+             
+         }
+
+
+
+
+          console.log(name);
+        
+
+      })
+    //   .catch(err => {
+    //       console.error(err);
+    //   });
+//   .catch(err => {
+//   console.error(err);
+// });
 }
 
-// Only shows on the webpage 2 at a time. Will work on time delay process with bryan
-// genreRequest("/chart/popular/genre/family"); // Harry Potter and the Sorcerer's Stone
-// genreRequest("/chart/popular/genre/musical"); //Sing
-// genreRequest("/chart/popular/genre/comedy"); //Love Hard
-// genreRequest("/chart/popular/genre/mystery"); //Midsommar
-// genreRequest("/chart/popular/genre/thriller"); // Jurassic World: Dominion
-// genreRequest("/chart/popular/genre/drama"); // The Humans
-// genreRequest("/chart/popular/genre/fantasy"); // A Boy Called Christmas
-// genreRequest("/chart/popular/genre/history"); //The Courier
-// genreRequest("/chart/popular/genre/horror"); //The Black Phone
-// genreRequest("/chart/popular/genre/sci-fi"); // The Matrix Resurrections
-// genreRequest("/chart/popular/genre/action"); //The Matrix Resurrections
-// genreRequest("/chart/popular/genre/animation"); // The polar express
-// genreRequest("/chart/popular/genre/adventure"); //Encanto
-// genreRequest("/chart/popular/genre/romance"); //a castle for christmas
-
-
-
+// fetch("https://tasty.p.rapidapi.com/recipes/detail?id=2270", {
+// 	"method": "GET",
+// 	"headers": {
+// 		"x-rapidapi-host": "tasty.p.rapidapi.com",
+// 		"x-rapidapi-key": "6367609f03mshe6a3e9c1f4e6ba0p12185cjsn01a783ae2181"
+// 	}
+// })
+// .then(response => {
+// 	return response.json()
+// }) .then(response => {
+// 	console.log(response);
+// })
+// .catch(err => {
+// 	console.error(err);
+// });
